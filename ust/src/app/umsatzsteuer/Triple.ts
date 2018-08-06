8c6fe2636454288b442bf4bea3ebebdb43f4e92d
kropiunik <stefan.kropiunik@gmail.com>
2018-08-06 15:18:07 +0200

    numToString mit ,00 hinten

--- ust/src/app/umsatzsteuer/Triple.ts
+++ ust/src/app/umsatzsteuer/Triple.ts
@@ -1,29 +1,28 @@
 import { RandomNumberGenerator } from '../services/RandomNumberGenerator.service';
 
-export class Triple{
-    text: string = "";
-    netto: number = 0;
-    ust: number = 0;
-    brutto: number = 0;
-    code: number = 0;
-    seed: number = -1;
+export class Triple {
+    text = '';
+    netto = 0;
+    ust = 0;
+    brutto = 0;
+    code = 0;
+    seed = -1;
     rng: RandomNumberGenerator;
 
     constructor(randomNumberGenerator: RandomNumberGenerator) {
         this.rng = randomNumberGenerator;
-        let choice = this.rng.nextRange(1,4);        
-        
-        if(choice == 1){
+        let choice = this.rng.nextRange(1, 4);
+        if (choice === 1) {
             this.code = 10;
-        }else if(choice == 2){
+        } else if (choice === 2) {
             this.code = 13;
-        }else{
+        } else {
             this.code = 20;
         }
 
-        choice = this.rng.nextRange(1,4); 
+        choice = this.rng.nextRange(1, 4);
 
-        switch (choice){
+        switch (choice) {
             case 1: {
                 this.initBrutto(); break;
             }
@@ -34,30 +33,27 @@ export class Triple{
                 this.initUSt(); break;
             }
         }
-       
     }
 
-
-
-    initBrutto(){ 
+    initBrutto() {
         this.brutto = this.roundTo2(this.rng.nextFloat() * 5000) + 1;
         this.netto = this.roundTo2(this.brutto / (100 + this.code)  * 100);
         this.ust = this.roundTo2(this.brutto - this.netto);
 
-        let choice = this.rng.nextRange(1,5);   
-     
-        switch (choice){
+        const choice = this.rng.nextRange(1, 5);
+
+        switch (choice) {
             case 1: {
-                this.text = this.numToString(this.brutto) + " inkl. " + this.code + " % USt"; break;
+                this.text = this.numToString(this.brutto) + ' inkl. ' + this.code + ' % USt'; break;
             }
             case 2: {
-                this.text = "Brutto: " + this.numToString(this.brutto) + " (" + this.code + " % USt)"; break;
+                this.text = 'Brutto: ' + this.numToString(this.brutto) + ' ( inkl.' + this.code + ' % USt)'; break;
             }
             case 3: {
-                this.text = this.numToString(this.brutto) + " brutto (" + this.code + " % USt)"; break
+                this.text = this.numToString(this.brutto) + ' brutto (' + this.code + ' % USt)'; break;
             }
             case 4: {
-                this.text = this.numToString(this.brutto) + " = (" + (this.code+100) + " % USt)"; break
+                this.text = this.numToString(this.brutto) + ' = (' + (this.code + 100) + ' %)'; break;
             }
         }
     }
@@ -66,27 +62,27 @@ export class Triple{
         this.netto = this.roundTo2(this.rng.nextFloat() * 5000) + 1;
         this.brutto = this.roundTo2(this.netto / 100 * (100 + this.code));
         this.ust = this.roundTo2(this.brutto - this.netto);
-        
-        let choice = this.rng.nextRange(1,5);
-    
-        switch (choice){
+
+        const choice = this.rng.nextRange(1, 5);
+
+        switch (choice) {
             case 1: {
-                this.text = "Netto: " + this.numToString(this.netto) + " exkl. " + this.code + " % USt";  break;
+                this.text = 'Netto: ' + this.numToString(this.netto) + ' exkl. ' + this.code + ' % USt';  break;
             }
             case 2: {
-                this.text = this.numToString(this.netto) + " (exkl. " + this.code + " %)"; break;
+                this.text = this.numToString(this.netto) + ' (exkl. ' + this.code + ' %)'; break;
             }
             case 3: {
-                this.text =  this.numToString(this.netto) + " + " + this.code + " % USt";  break;
+                this.text =  this.numToString(this.netto) + ' + ' + this.code + ' % USt';  break;
             }
             case 4: {
-                this.text = this.numToString(this.netto) + " netto (" + this.code + " % USt)";  break;
+                this.text = this.numToString(this.netto) + ' netto (' + this.code + ' % USt)';  break;
 
             }
         }
     }
 
-    initUSt(){
+    initUSt() {
         this.ust = this.roundTo2(this.rng.nextFloat() * 5000) + 1;
         this.netto = this.roundTo2(this.ust / ( this.code)  * 100);
         this.brutto = this.roundTo2(this.ust  + this.netto);
@@ -121,21 +117,26 @@ export class Triple{
         return n;
     }
 
-     numToString(num : number) : string {
+     numToString(num: number): string {
         let ret = String(num);
-        ret = ret.replace(".", ",");
-        let splitted = ret.split(",");
-        //insert kilo seperator e.g.: 1000 -> 1.000
+        ret = ret.replace('.', ',');
+        const splitted = ret.split(',');
+        // insert kilo seperator e.g.: 1000 -> 1.000
         let count = 1;
-        for(let i = splitted[0].length-1; i > 0; i--){
-            if(count == 3){
-                ret = ret.slice(0, i) + "." + ret.slice(i);
+        for (let i = splitted[0].length - 1; i > 0; i--) {
+            if (count === 3) {
+                ret = ret.slice(0, i) + '.' + ret.slice(i);
                 count = 0;
-            }else{
+            } else {
                 count ++;
             }
         }
-        
+        if (!ret.includes(',')) {
+            ret = ret + ',00';
+        }
+        if (ret.search(',') === (ret.length - 2)) {
+            ret = ret + '0';
+        }
 
         return ret;
     }
