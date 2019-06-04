a485fc4bef803a4ac20c441f970db4b86962224f
Hans Kropiunik <h.kropiunik@KROPIUNIK.local>
2019-06-04 14:39:32 +0200

    mongo-server NetworkError : Timeout (Port blocked ?)

--- mongo-server/server.js
+++ mongo-server/server.js
@@ -1,10 +1,10 @@
 
-const exress = require("express");
+const express = require("express");
 const app = express();
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://tc:tceluzuz@tc-p6gdc.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://tc:Mu593TcX8xKwjRAy@tc-p6gdc.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.listen(3000, () => {
@@ -12,12 +12,17 @@ app.listen(3000, () => {
   console.log("listening on 3000");
 
   client.connect(err => {
-    const collection = client.db("tc").collection("tc");
+    console.log("try to connenct to mongodb");
+    if(!err){
+    const collection = client.db("tc").collection("users");
     // perform actions on the collection object
-    collection.insert("Huber");
     console.log(collection);
-    //client.close();
-    console.log(error);
+    client.close();
+    }else{
+      console.log("OJE:" + err);
+    }
   });
   
 });
+
+
