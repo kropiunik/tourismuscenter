669d16148766b662c0d4c72d9f138f2b0d1f4ea7
kropiunik <stefan.kropiunik@gmail.com>
2020-01-09 18:49:12 +0100

    server added + atlas db

--- mongo-server/server.js
+++ mongo-server/server.js
@@ -1,28 +1,35 @@
+// server.js
 
 const express = require("express");
-const app = express();
+const server = express();
 
+const body_parser = require("body-parser");
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://tc:Mu593TcX8xKwjRAy@tc-p6gdc.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
 
-app.listen(3000, () => {
+const port = 4000;
 
-  console.log("listening on 3000");
+// << db setup >>
+const db = require("./db");
+const dbName = "data";
+const collectionName = "datum";
 
-  client.connect(err => {
-    console.log("try to connenct to mongodb");
-    if(!err){
-    const collection = client.db("tc").collection("users");
-    // perform actions on the collection object
-    console.log(collection);
-    client.close();
-    }else{
-      console.log("OJE:" + err);
-    }
+// << db init >>
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+  // get all items
+  dbCollection.find().toArray(function(err, result) {
+      if (err) throw err;
+        console.log(result);
   });
-  
+
+  // << db CRUD routes >>
+
+}, function(err) { // failureCallback
+  throw (err);
+});
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
 });
 
 
